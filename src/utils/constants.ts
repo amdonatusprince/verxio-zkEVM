a2c5d202441bcd654a5128bc827b5dfee1cac67a
amdonatusprince <donatusprince@gmail.com>
2023-12-05 14:58:51 +0100

    feat: verxio initialize

---
+++ src/utils/constants.ts
@@ -0,0 +1,12 @@
+
+import { AddrMapType, MapType } from './types';
+
+export const registryAddress: AddrMapType = {
+  0: '0x0',
+  1001: '0x4838854e5150E4345Fb4Ae837E9FcCa40D51F3Fe'
+  
+}
+
+export const explorer: MapType = {
+  1001: 'baobab.scope.klaytn.com',
+}
